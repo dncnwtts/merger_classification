f3e0f5dfee1e804849069a67e3772025aff58e97
Duncan Watts <dwatts@pha.jhu.edu>
2020-04-27 01:31:29 +0000

    Estimate of probability a galaxy given people's answers makes sense.

--- accuracy_analysis.py
+++ accuracy_analysis.py
@@ -77,29 +77,6 @@ def f_b(k, n, p, thresh=100):
     #logf_b = logf1
     return logf_b
 
-'''
-def L(Ns, rj, rjp, N_M, Ntot, high_acc=False):
-    L = 0
-    kmin = Ns.min()+N_M-Ntot
-    kmax = Ntot
-    print(kmin, kmax)
-    if high_acc:
-        #N_Mks = np.linspace(kmin,kmax,1001)
-        N_Mks = np.linspace(0,Ntot,1001)
-    else:
-        #Ns = Ns.astype(int)
-        #N_Mks = np.arange(int(kmin),int(kmax+1))
-        N_Mks = np.arange(0.5,Ntot+1)
-    for k in N_Mks:
-        L1 = np.zeros(len(Ns))-np.inf
-        L2 = np.zeros(len(Ns))-np.inf
-        inds = ((Ns >= k) & ((Ntot-Ns) >= (N_M-k)))
-        L1[inds] = f_b(k, Ns[inds], rj)
-        L2[inds] = f_b(N_M-k, Ntot-Ns[inds], 1-rjp)
-        L += np.exp(L1+L2)
-    return L/sum(L*np.diff(Ns)[0])
-'''
-
 def P(N_Mhat, N_M, N_I, r_I, r_M):
     P = 0
     for N_Mhat1 in np.arange(0, N_M+1):
@@ -133,39 +110,177 @@ def test1(N_M=0, N=10, r_I=0.7, r_M=0.8,
     plt.xlabel(r'$\hat N_M$')
 
 
+    # Expected number of correctly identified mergers
+    N_M1 = r_M * N_M
+    # Expected number of isolated galaxies identified as merger
+    N_M2 = (1-r_I)*(N-N_M)
+
+
     f_M = N_M/N
     N_input, N_Mhats, r_Ms, r_Is = make_sample(N, f_M, n_survey, N_M=0, width_I=0.00,
             width_M=0.00, I=r_I, M=r_M)
     bins = np.arange(N+1)-0.5
     plt.hist(N_Mhats, bins=bins, density=True)
+    plt.axvline(N_M, label='Number of input mergers', color='b')
+    plt.axvline(N_M1 + N_M2, label='Expected number of measured mergers',
+            color='r')
+    plt.legend(loc='best')
+    plt.xlabel(r'$\hat N_M$')
+    plt.ylabel(r'$P(\hat N_M\mid N_M, N_I, r_I, r_M$')
     return
 
 
-def test2(N_M=0, N=10, r_I=0.7, r_M=0.8):
+def test2(N_M=0, N=10, r_I=0.7, r_M=0.8, n_survey=25):
     '''
     Given a measurement N_Mhat, what is the probability that N_M is recovered?
     '''
     f_M = N_M/N
-    n_survey = 25
-    N_input, N_Mhats, r_Ms, r_Is = make_sample(N, f_M, n_survey, N_M=0, width_I=0.00,
+    N_input, N_Mhats, r_Ms, r_Is = make_sample(N, f_M, n_survey, N_M=N_M, width_I=0.00,
             width_M=0.00, I=r_I, M=r_M)
 
 
 
+    Lis = []
     for N_Mhat in N_Mhats:
         N_Ms = np.arange(N+1)
-        plt.plot(N_Ms, L(N_Ms, r_I, r_M, N_Mhat, N), color='k', alpha=0.5)
+        Lis.append(L(N_Ms, r_I, r_M, N_Mhat, N))
+        plt.plot(N_Ms, Lis[-1], color='k', alpha=0.5, zorder=2)
 
+    Lis = np.array(Lis)
+    logL_tot = sum(np.log(Lis))
+    L_tot = np.exp(logL_tot - logL_tot.max())
+    plt.plot(N_Ms, L_tot, lw=5)
+    plt.axvline(N_M, linestyle='--', label='True Input')
+    plt.xlabel(r'$N_M$')
+    plt.ylabel(r'$P(N_M\mid \{\hat N_M\},\{r_I\},\{r_J\}, N_\mathrm{tot})$')
+    plt.legend(loc='best')
+
+    return
+
+
+def test3(f_M=0.3):
+    '''
+    Attempting to show how off a merger fraction could be as a function of
+    accuracy.
+    '''
+    r_M = np.linspace(0,1)
+    r_I = np.linspace(0,1)
+    r_M, r_I = np.meshgrid(r_M, r_I)
+
+
+    f_MHat = r_M*f_M + (1-r_I)*(1-f_M)
+    plt.figure()
+    plt.pcolormesh(r_M, r_I, f_MHat, vmin=0, vmax=1)
+    plt.colorbar(label=r'$\hat f_M$')
+    plt.xlabel(r'$r_M$')
+    plt.ylabel(r'$r_I$')
+    plt.title(r'$f_M={0}$'.format(f_M))
+    return
+
+
+
+def test4(f_M=0.3, N=100):
+    '''
+    Attempting to show how off a merger fraction could be as a function of
+    accuracy.
+    '''
+    r_M = np.linspace(0,1)
+    r_I = np.linspace(0,1)
+    r_M, r_I = np.meshgrid(r_M, r_I)
+
+    N_M = f_M*N
+    N_I = (1-f_M)*N
+
+
+
+    N_MHat = r_M*N_M + (1-r_I)*N_I
+    plt.figure()
+    plt.pcolormesh(r_M, r_I, N_MHat)
+    plt.colorbar(label=r'$\hat N_M$')
+    plt.xlabel(r'$r_M$')
+    plt.ylabel(r'$r_I$')
+    plt.title(r'$f_M={0}$'.format(f_M))
+    return
+
+
+
+def test5():
+    '''
+    I would like to get the probability that a galaxy is a merger given an
+    individual's classification.
+    '''
+    # Let's say a respondent says that a given galaxy is a merger.
+    r_M = np.linspace(0,1)
+    r_I = 0.8
+    p_Mhat_M = r_M
+    p_Mhat_I = 1-r_I
+
+    p_M = p_Mhat_M/(p_Mhat_M + p_Mhat_I)
+    plt.plot(r_M, p_M)
+    plt.xlabel(r'$r_M$')
+    plt.ylabel(r'$p(\mathrm{{merger}}\mid\mathrm{{observed merger}},r_I={0},r_M)$'.format(r_I))
+    
+
+    plt.figure()
+    r_M = 0.7
+    r_I = np.linspace(0,1)
+    p_Mhat_M = r_M
+    p_Mhat_I = 1-r_I
+
+    p_M = p_Mhat_M/(p_Mhat_M + p_Mhat_I)
+    plt.plot(r_I, p_M)
+    plt.xlabel(r'$r_I$')
+    plt.ylabel(r'$p(\mathrm{{merger}}\mid\mathrm{{observed merger}},r_M={0},r_I)$'.format(r_M))
+    plt.show()
+
+    return
+
+
+
+def test6():
+    '''
+    Let us simulate a group of observers with known accuracies, and get the
+    probability of a galaxy actually being a merger.
+    '''
+    N = 1
+    N_M = 1
+    f_M = 1
+    n_survey = 5
+    N_input, N_Mhats, r_Ms, r_Is = make_sample(N, f_M, n_survey, N_M=N_M, I=0.8,
+            M=0.8)
+    p_Mhat_M = r_Ms
+    p_Mhat_I = 1-r_Is
+    p_M = p_Mhat_M/(p_Mhat_M + p_Mhat_I)
+    plt.hist(p_M)
+
+
+    print(N_input)
+    print(N_Mhats)
+    print(r_Ms)
+    print(r_Is)
+
+
+    P = 1
+    den_M = 1
+    den_I = 1
+    for i in range(len(N_Mhats)):
+        den_M *= r_Ms[i]
+        den_I *= 1-r_Is[i]
+        P *= r_Ms[i]
+    den = den_M + den_I
+    print('Probability of being a merger', P/den)
+    print('Average answer', N_Mhats.mean())
+    plt.show()
 
     return
 
 if __name__ == '__main__':
+    '''
     N = 50
     Nhat = np.arange(N+1)
     r_M = 0.8
     r_I = 0.7
    
-    '''
     plt.figure()
     test1(N=10, N_M=0, r_M=r_M, r_I=r_I)
     plt.figure()
@@ -173,10 +288,14 @@ if __name__ == '__main__':
     plt.figure()
     test1(N=100, N_M=0, n_survey=2000)
     plt.show()
-    '''
 
-    test2(N=100)
+    test2(N=50, N_M=50)
     plt.show()
 
+    test3()
+    test3(f_M=0.1)
+    test3(f_M=0.7)
+    plt.show()
+    '''
 
-
+    test6()
